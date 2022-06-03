d76e63473d2ab542f684a864c7cc99bb6ad497ff
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 13:59:59 +0100

    Implementing movie details view

--- src/views/Movie.tsx
+++ src/views/Movie.tsx
@@ -1,8 +1,24 @@
 import { useParams } from 'react-router-dom';
+import { getMovie, movieId } from '@/store/movies';
+import { useAtom } from 'jotai';
+import BackButton from '@/components/buttons/BackButton';
 
 const Movie = () => {
-  const { id } = useParams();
-  return <h1>Movie: {id}</h1>;
+  const { id = '' } = useParams();
+  const [movie] = useAtom(getMovie);
+  const [, setMoveId] = useAtom(movieId);
+
+  setMoveId(id);
+
+  if (!movie) return <div>Movie not found</div>;
+
+  return (
+    <>
+      <BackButton />
+
+      <h1>{movie.title}</h1>
+    </>
+  );
 };
 
 export default Movie;
