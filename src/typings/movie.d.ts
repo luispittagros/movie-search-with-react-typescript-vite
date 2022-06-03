593e77cfcedf298cde8f6f9bee678d532074734c
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 13:54:41 +0100

    create omdb api response typings

--- src/typings/movie.d.ts
+++ src/typings/movie.d.ts
@@ -1,7 +1,22 @@
 type Movie = {
   id: string;
   title: string;
-  year: number;
+  year: string;
   poster: string;
   isFavorite?: boolean;
+};
+
+type OMDBMovie = {
+  Title: string;
+  Year: string;
+  Poster: string;
+  Type: string;
+  imdbID: string;
+};
+
+type OMDBSearchResponse = {
+  Response: 'True' | 'False';
+  Search?: OMDBMovie[];
+  totalResults?: string;
+  Error?: string;
 };
