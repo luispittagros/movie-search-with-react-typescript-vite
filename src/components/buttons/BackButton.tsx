4dab76034b835bb6f5837fc7af6ab99dbdde25db
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 13:49:00 +0100

    Implement back button

---
+++ src/components/buttons/BackButton.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Arrow } from '@/assets/svg/icons/icon-arrow.svg';
+
+import '@/components/buttons/BackButton.scss';
+
+const BackButton: FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button className="back-button" onClick={handleClick} type="button">
+      <Arrow />
+    </button>
+  );
+};
+export default BackButton;
