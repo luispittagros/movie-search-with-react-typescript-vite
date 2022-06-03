92aef2d48590fb056c121b228b34ffdcfdf6f8ad
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 13:56:28 +0100

    remove toggle favorite from movies list, cleanup

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,36 +1,16 @@
-import { useCallback } from 'react';
 import { useAtom } from 'jotai';
-import moviesStore from '@/store/movies';
+import { moviesStore } from '@/store/movies';
 import MovieCard from '@/components/movies/MovieCard';
 import '@/components/movies/Movies.scss';
+import { FC } from 'react';
 
-const Movies = () => {
-  const [movies, setMovies] = useAtom(moviesStore);
-
-  const toggleFavorite = (id: string) => {
-    setMovies((moviesList) => {
-      const newMovies = [...moviesList];
-
-      const movieFound = newMovies.find((movie) => movie.id === id);
-
-      if (movieFound) {
-        movieFound.isFavorite = !movieFound.isFavorite;
-      }
-
-      return newMovies;
-    });
-  };
-
-  const handleFavorite = useCallback(toggleFavorite, [setMovies]);
+const Movies: FC = () => {
+  const [movies] = useAtom(moviesStore);
 
   return (
     <ul className="movies">
       {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          handleFavorite={handleFavorite}
-        />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </ul>
   );
