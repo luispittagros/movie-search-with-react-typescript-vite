0f6217d9050dcbed1d95ab341f7c293c1d9175fd
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 22:11:24 +0100

    Implementing movie details

--- src/views/Movie.tsx
+++ src/views/Movie.tsx
@@ -1,14 +1,49 @@
 import { useParams } from 'react-router-dom';
-import { getMovie, movieId } from '@/store/movies';
+import { movieStore } from '@/store/movies';
 import { useAtom } from 'jotai';
+import { fetchMovie } from '@/api/movies';
 import BackButton from '@/components/buttons/BackButton';
+import { FC, useEffect } from 'react';
 
-const Movie = () => {
+import { ReactComponent as IMDBLogo } from '@/assets/svg/logos/logo-imdb.svg';
+import { ReactComponent as RottenTomatoesLogo } from '@/assets/svg/logos/logo-rotten-tomatoes.svg';
+
+import '@/views/Movie.scss';
+import LogoLabel from '@/components/labels/LogoLabel';
+import TextLabel from '@/components/labels/TextLabel';
+
+const Movie: FC = () => {
   const { id = '' } = useParams();
-  const [movie] = useAtom(getMovie);
-  const [, setMoveId] = useAtom(movieId);
+  const [movie, setMovie] = useAtom(movieStore);
 
-  setMoveId(id);
+  useEffect(() => {
+    const fetchData = async (): Promise<Movie> => {
+      const { data } = await fetchMovie(id);
+
+      return {
+        id,
+        title: data.Title,
+        year: data.Year,
+        runtime: data.Runtime,
+        genres: data.Genre.split(', '),
+        directors: data.Director.split(', '),
+        cast: data.Actors.split(', '),
+        plot: data.Plot,
+        poster: data.Poster,
+        rated: data.Rated,
+        imdbRating: data.Ratings.find(
+          (rating) => rating.Source === 'Internet Movie Database',
+        )?.Value,
+        rottenTomatoes: data.Ratings.find(
+          (rating) => rating.Source === 'Rotten Tomatoes',
+        )?.Value,
+      };
+    };
+
+    fetchData()
+      .then((data) => setMovie(data))
+      .catch(console.error);
+  }, [setMovie, id]);
 
   if (!movie) return <div>Movie not found</div>;
 
@@ -16,7 +51,71 @@ const Movie = () => {
     <>
       <BackButton />
 
-      <h1>{movie.title}</h1>
+      <div className="movie">
+        <div className="movie__info">
+          <div className="movie__meta">
+            <TextLabel>{movie.runtime}</TextLabel> ·
+            <TextLabel>{movie.year}</TextLabel> ·
+            <TextLabel filled>{movie.rated}</TextLabel>
+          </div>
+
+          <h1 className="movie__title">{movie.title}</h1>
+
+          <div className="movie__rating">
+            <LogoLabel logo={<IMDBLogo />} color="--yellow">
+              {movie.imdbRating}
+            </LogoLabel>
+
+            {movie.rottenTomatoes && (
+              <LogoLabel logo={<RottenTomatoesLogo />} color="--red">
+                {movie.rottenTomatoes}
+              </LogoLabel>
+            )}
+          </div>
+
+          <div className="movie__plot">
+            <TextLabel>Plot</TextLabel>
+            <p>{movie.plot}</p>
+          </div>
+
+          <div className="movie__details">
+            <div className="movie__details-item">
+              <TextLabel>Cast</TextLabel>
+
+              <ul>
+                {movie.cast?.map((cast) => (
+                  <li key={cast}>{cast}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="movie__details-item">
+              <TextLabel>Genres</TextLabel>
+
+              <ul>
+                {movie.genres?.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="movie__details-item">
+              <TextLabel>Director</TextLabel>
+
+              <ul>
+                {movie.directors?.map((director) => (
+                  <li key={director}>{director}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="movie__poster"
+          style={{ backgroundImage: `url(${movie.poster})` }}
+        />
+      </div>
     </>
   );
 };
