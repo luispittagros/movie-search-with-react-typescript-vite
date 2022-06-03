37a190ecd84663f808636a99afe8ffc3b3a737a6
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-03 13:57:42 +0100

    Implement search bar

---
+++ src/components/search/SearchBar.tsx
@@ -0,0 +1,55 @@
+import { useAtom } from 'jotai';
+import { moviesStore, loadingMovies, searchTerm } from '@/store/movies';
+import '@/components/search/SearchBar.scss';
+import { ChangeEvent, useCallback } from 'react';
+import searchMovies from '@/api/movies';
+import debounce from 'lodash.debounce';
+
+const SearchBar = () => {
+  const [, setMovies] = useAtom(moviesStore);
+  const [, setLoading] = useAtom(loadingMovies);
+  const [, setSearch] = useAtom(searchTerm);
+
+  const debouncedHandleChange = useCallback(
+    debounce(async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
+      const { value } = e.target;
+
+      setSearch(value);
+
+      try {
+        setLoading(true);
+
+        const { data } = await searchMovies(value);
+
+        const results = data.Search?.map<Movie>((movie) => ({
+          id: movie.imdbID,
+          title: movie.Title,
+          year: movie.Year,
+          poster: movie.Poster,
+          isFavorite: false,
+        }));
+
+        if (results) {
+          setMovies(results);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }, 500),
+    [debounce, setMovies, setLoading, setSearch],
+  );
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search movies..."
+        onChange={debouncedHandleChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
