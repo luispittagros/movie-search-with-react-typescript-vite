4ce280f22f4850e4dc17d9702bff0984cc5c9930
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-05 02:47:05 +0100

    use infinite loading hook

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,19 +1,42 @@
 import { useAtom } from 'jotai';
-import { loadingAtom, moviesAtom } from '@/store/movies';
+
+import {
+  hasMoreAtom,
+  loadingAtom,
+  moviesAtom,
+  searchPageAtom,
+  searchQueryAtom,
+} from '@/store/movies';
 import MovieCard from '@/components/movies/MovieCard';
 import '@/components/movies/Movies.scss';
 import Loader from '@/components/loader/Loader';
+import { useEffect, useRef } from 'react';
+import useInfiniteLoading from '@/hooks/useInfiniteLoading';
 
 const Movies = () => {
   const [movies] = useAtom(moviesAtom);
   const [loading] = useAtom(loadingAtom);
 
-  if (loading) return <Loader />;
+  const [searchQuery] = useAtom(searchQueryAtom);
+  const [searchPage, setSearchPage] = useAtom(searchPageAtom);
+  const [hasMore] = useAtom(hasMoreAtom);
+
+  const element = useRef<HTMLUListElement | null>(null);
+
+  const [loadMore] = useInfiniteLoading(element);
+
+  useEffect(() => {
+    if (loadMore && !loading && hasMore && searchQuery) {
+      setSearchPage(searchPage + 1);
+    }
+  }, [loadMore, movies]);
+
+  if (loading && !movies.length) return <Loader />;
 
   return (
-    <ul className="movies">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+    <ul className="movies" ref={element}>
+      {movies.map((movie, index) => (
+        <MovieCard key={`${movie.id}-${index}`} movie={movie} />
       ))}
     </ul>
   );
